d95eb2b6064530ec328f9553d961323d06405471
BB <bbislv12@gmail.com>
2026-01-27 15:49:44 +0300

    Initial commit

---
+++ src/test/java/com/skypro/skyshop/SkyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.skyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
